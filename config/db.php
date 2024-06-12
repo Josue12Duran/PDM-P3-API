08c297b8785a43437044f7554bb3e2cba3c28c35
JD <JDDuran@innovacion.gob.sv>
2024-06-12 14:08:55 -0600

    chore: Update database connection details in config/db.php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 
 class DB {
-    private $host = 'localhost:33060';
-    private $user = 'root';
+    private $host = 'localhost';
+    private $user = 'id22309351_root';
     private $pass = 'Api_DC20026';
-    private $dbname = 'casa_top';
+    private $dbname = 'id22309351_casa_top';
 
     public function connect() {
         $mysql_connect_str = "mysql:host=$this->host;dbname=$this->dbname";
