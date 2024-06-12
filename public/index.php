65ca3b6723fd41a92376fc79b64f56960b800471
JD <JDDuran@innovacion.gob.sv>
2024-06-12 16:46:17 -0600

    chore: Remove unnecessary route for /hello/{name} in public/index.php

--- public/index.php
+++ public/index.php
@@ -8,12 +8,6 @@ require __DIR__ . '/../config/db.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
 // Rutas para propietarios
 require __DIR__ . '/../routes/propietarios.php';
 
